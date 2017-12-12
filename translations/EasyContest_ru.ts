32ef2a14bb350ba792c4a1306d5b2bc82988b85f
Александр Ящук <jashhuk@vistgroup.ru>
2017-12-12 18:39:25 +0300

    Сделаны переводы лейблов

--- translations/EasyContest_ru.ts
+++ translations/EasyContest_ru.ts
@@ -1,64 +1,64 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../source/gui/mainwindow.ui" line="38"/>
         <source>Easy Contest</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Easy Contest</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.ui" line="55"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Соединение</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="69"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Настройки</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="88"/>
         <source>Contest settings:
 	</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Настройки контеста</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="106"/>
         <location filename="../source/gui/mainwindow.cpp" line="158"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">О программе</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="114"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Выход</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="130"/>
         <source>open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">открыто</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="135"/>
         <source>close</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">закрыто</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="142"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Подключено</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="145"/>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Отключено</translation>
     </message>
     <message>
         <location filename="../source/gui/mainwindow.cpp" line="158"/>
         <source>Contest-log program for CW contests&lt;BR&gt;&lt;FONT COLOR=&apos;BLUE&apos;&gt;Version&lt;/FONT&gt;: &lt;B&gt;&lt;FONT COLOR=&apos;RED&apos;&gt;%1&lt;/FONT&gt;&lt;/B&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Программа контест-лог для CW соревнований&lt;BR&gt;&lt;FONT COLOR=&apos;BLUE&apos;&gt;Версия&lt;/FONT&gt;: &lt;B&gt;&lt;FONT COLOR=&apos;RED&apos;&gt;%1&lt;/FONT&gt;&lt;/B&gt;</translation>
     </message>
 </context>
 </TS>
