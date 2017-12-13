f8abf07cec920445a48e42d0ce81e964ff825ddb
Aleksandr Jashhuk <richmanfx@yandex.ru>
2017-12-13 22:32:22 +0300

    Задействован русский файл локализации

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,15 @@
 #include <QApplication>
+#include <QTranslator>
 #include "gui/mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     QTranslator translator;
-//    translator.load(":/translations/EasyContest_" + QLocale::system().name());
-    translator.load(":/translations/EasyContest_ru");
-    a.installTranslator(&translator);
+    translator.load("EasyContest_ru");
+    qApp->installTranslator(&translator);
+
     MainWindow w(0, "EasyContest" );
 
     // Только кнопки окна "свернуть" и "закрыть"
